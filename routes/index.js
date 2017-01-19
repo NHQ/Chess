ee3f328aed4f02fd959ff57e4563df0e3de136e3
Tree <tree@multiplexed.be>
2017-01-18 19:19:32 -0800

    push n

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 
-var origin = process.env.NODE_ENV === 'production' ? '74.207.246.247' : '127.0.0.1'
+var origin = process.argv[2] || "0.0.0.0"
+var port = Number(process.argv[3]) || 3301
 
 
 exports.board = function(req, res){
-  res.render('index', {origin: origin});
+  res.render('index', {origin: origin, port: port});
 };
 exports.index = function(req, res){
   var gameID = Math.ceil( Math.random() * Date.now() )
